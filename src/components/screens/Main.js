14ec2ade46871fb551aa4414434a830c3204cb22
ShlomoRuckban <Shlomoruckban@gmail.com>
2022-12-20 22:07:25 +0200

    WIP added needed packages

--- src/components/screens/Main.js
+++ src/components/screens/Main.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import Reactotron from 'reactotron-react-js'
 
 class Main extends Component {
   render() {
+    Reactotron.display({
+      name: 'KNOCK KNOCK',
+      preview: 'Who\'s there?',
+      value: 'Orange.'
+    })
     return <div>test Main</div>;
   }
 }
